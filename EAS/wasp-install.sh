d6066db1315c3c5ebeae948180c0922d80ad8bcd
Jawad Bish <Jawad.bish@gmail.com>
2024-11-24 14:56:04 +0200

    Initial commit

---
+++ EAS/wasp-install.sh
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Begin Jekyll SEO tag v2.8.0 -->
+<title>get-wasp</title>
+<meta name="generator" content="Jekyll v3.9.3" />
+<meta property="og:title" content="get-wasp" />
+<meta property="og:locale" content="en_US" />
+<link rel="canonical" href="https://get.wasp-lang.dev/" />
+<meta property="og:url" content="https://get.wasp-lang.dev/" />
+<meta property="og:site_name" content="get-wasp" />
+<meta property="og:type" content="website" />
+<meta name="twitter:card" content="summary" />
+<meta property="twitter:title" content="get-wasp" />
+<script type="application/ld+json">
+{"@context":"https://schema.org","@type":"WebSite","headline":"get-wasp","name":"get-wasp","url":"https://get.wasp-lang.dev/"}</script>
+<!-- End Jekyll SEO tag -->
+
+    <link rel="stylesheet" href="/assets/css/style.css?v=e5e8572647af01b665d60778608b3014850a3daf">
+    <!-- start custom head snippets, customize with your own _includes/head-custom.html file -->
+
+<!-- Setup Google Analytics -->
+
+
+
+<!-- You can set your favicon here -->
+<!-- link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" -->
+
+<!-- end custom head snippets -->
+
+  </head>
+  <body>
+    <div class="container-lg px-3 my-5 markdown-body">
+      
+
+      <h1 id="get-wasp">get-wasp</h1>
+
+<p>This repo is used as a way to publish static wasp resources, primarily installer script.</p>
+
+<p>Anything that you put here in master branch will be available under get.wasp-lang.dev/ (via Github Pages).</p>
+
+<p>Therefore, installer.sh will be available as get.wasp-lang.dev/installer.sh .</p>
+
+<h2 id="development">Development</h2>
+
+<p>Test with <code class="language-plaintext highlighter-rouge">dash installer.sh</code>, to ensure it works on variety with <code class="language-plaintext highlighter-rouge">sh</code>, not just <code class="language-plaintext highlighter-rouge">bash</code> (dash is implementation of <code class="language-plaintext highlighter-rouge">sh</code>, so simpler than <code class="language-plaintext highlighter-rouge">bash</code>, and is a default shell on ubuntu).</p>
+
+<p>Also, use shellcheck to check the code: <code class="language-plaintext highlighter-rouge">shellcheck installer.sh</code>.</p>
+
+
+      
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/anchor-js/4.1.0/anchor.min.js" integrity="sha256-lZaRhKri35AyJSypXXs4o6OPFTbTmUoltBbDCbdzegg=" crossorigin="anonymous"></script>
+    <script>anchors.add();</script>
+  </body>
+</html>
