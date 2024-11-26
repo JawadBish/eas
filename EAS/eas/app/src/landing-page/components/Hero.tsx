b07baea00cd44bbf9264fbc2208d3e2c0852312e
jawadb <jawad.bish@gmail.com>
2024-11-26 06:26:24 +0200

    change color

--- EAS/eas/app/src/landing-page/components/Hero.tsx
+++ EAS/eas/app/src/landing-page/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
             <div className='mt-10 flex items-center justify-center gap-x-6'>
               <a
                 href={DocsUrl}
-                className='rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-200 hover:ring-2 hover:ring-yellow-300 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:text-white'
+                className='rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-200 hover:ring-2 hover:ring-green-300 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:text-white'
               >
                 Get Started <span aria-hidden='true'>→</span>
               </a>
@@ -46,13 +46,13 @@ function TopGradient() {
   return (
     <div
       className='absolute top-0 right-0 -z-10 transform-gpu overflow-hidden w-full blur-3xl sm:top-0'
-    aria-hidden='true'
-  >
-    <div
-      className='aspect-[1020/880] w-[55rem] flex-none sm:right-1/4 sm:translate-x-1/2 dark:hidden bg-gradient-to-tr from-amber-400 to-purple-300 opacity-40'
-      style={{
-        clipPath: 'polygon(80% 20%, 90% 55%, 50% 100%, 70% 30%, 20% 50%, 50% 0)',
-      }}
+      aria-hidden='true'
+    >
+      <div
+        className='aspect-[1020/880] w-[55rem] flex-none sm:right-1/4 sm:translate-x-1/2 dark:hidden bg-gradient-to-tr from-amber-400 to-purple-300 opacity-40'
+        style={{
+          clipPath: 'polygon(80% 20%, 90% 55%, 50% 100%, 70% 30%, 20% 50%, 50% 0)',
+        }}
       />
     </div>
   );
@@ -62,14 +62,14 @@ function BottomGradient() {
   return (
     <div
       className='absolute inset-x-0 top-[calc(100%-40rem)] sm:top-[calc(100%-65rem)] -z-10 transform-gpu overflow-hidden blur-3xl'
-    aria-hidden='true'
-  >
-    <div
-      className='relative aspect-[1020/880] sm:-left-3/4 sm:translate-x-1/4 dark:hidden bg-gradient-to-br from-amber-400 to-purple-300  opacity-50 w-[72.1875rem]'
-      style={{
-        clipPath: 'ellipse(80% 30% at 80% 50%)',
-      }}
-    />
+      aria-hidden='true'
+    >
+      <div
+        className='relative aspect-[1020/880] sm:-left-3/4 sm:translate-x-1/4 dark:hidden bg-gradient-to-br from-amber-400 to-purple-300  opacity-50 w-[72.1875rem]'
+        style={{
+          clipPath: 'ellipse(80% 30% at 80% 50%)',
+        }}
+      />
     </div>
   );
 }
